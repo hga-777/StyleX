c5707f13b6267eb4cc794ee6261d3eac46a208ef
Hritik Anand <realhga@gmail.com:--global user.email>
2023-12-11 15:19:15 +0530

    database is created and connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to the Database');
+  })
+  .catch((err) => {
+    console.log('error is ${err}'); 
+  });
+ 
 const app = express();
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
