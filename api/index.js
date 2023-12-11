96cf36d0728b83b9381584a8fb7312ba09c5c2e4
Hritik Anand <realhga@gmail.com:--global user.email>
2023-12-11 19:13:55 +0530

    SignUp route is created

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js"
-
+import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose
@@ -16,8 +16,11 @@ mongoose
  
 const app = express();
 
+app.use(express.json()); //so that we can receive json file from when client posts something
+
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
 
 app.use('/api/user' ,userRoutes);
+app.use('/api/auth' ,authRoutes);
