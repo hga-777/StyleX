d303ec2851dd8ba25adfb2a49a9d85ae12e0fbb4
Hritik Anand <realhga@gmail.com:--global user.email>
2023-12-13 13:54:47 +0530

    SignIn page functionality is completed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
 
 export default function SignUp() {
     const [formData , setFormData] = useState({});
     const [error,setError] = useState(false);
     const [loading,setLoading] = useState(false);
+    const navigate = useNavigate();
     const handleChange = (e) =>{
         setFormData({...formData,[e.target.id] : e.target.value});  
     }
@@ -26,6 +27,7 @@ export default function SignUp() {
             setLoading(false);
             if(data.success === false) {setError(true); return;}
             setError(false);
+            navigate('/sign-in');
          }catch(error){
             setLoading(false);
             setError(true); 
