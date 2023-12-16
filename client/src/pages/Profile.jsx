212a707dfc6158cbb61f6f3e46420c8a33b5085c
Hritik Anand <realhga@gmail.com:--global user.email>
2023-12-16 18:23:46 +0530

    Sign Out user functionality is added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,7 +15,8 @@ import {
   updateUserFailure,
   deleteUserFailure,
   deleteUserStart,
-  deleteUserSuccess
+  deleteUserSuccess,
+  signOut
 } from '../redux/user/userSlice';
 
 export default function Profile() {
@@ -103,7 +104,15 @@ export default function Profile() {
       dispatch(deleteUserFailure(error));
     }
   };
-
+ 
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut())
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -158,7 +167,7 @@ export default function Profile() {
     </form>
     <div className="flex justify-between mt-5">
       <span className='text-red-700 cursor-pointer' onClick={handleDeleteAccount}>Delete Account</span>
-      <span className='text-red-700 cursor-pointer'>Sign out</span>
+      <span className='text-red-700 cursor-pointer' onClick={handleSignOut} >Sign out</span>
     </div>
     <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
       <p className='text-green-700 mt-5'>
