8a734ad7b110ff72855affaca7fd45e77b610d79
Hritik Anand <realhga@gmail.com:--global user.email>
2023-12-16 18:00:17 +0530

    Update user functionality is completed

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,9 +22,28 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload.message;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const {signInFailure , signInStart ,signInSuccess} = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} = userSlice.actions;
 
 export default userSlice.reducer;
